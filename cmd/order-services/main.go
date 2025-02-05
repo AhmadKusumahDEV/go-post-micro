f6db5cf4254ba93a7a88f5308ca2a3a2777e3653
MUHAMMAD-FATUR-SOFYAN-KUSUMAH <mfatur.fs@gmail.com>
2025-02-05 14:31:10 +0700

    try commit

---
+++ cmd/order-services/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+
+	muxHandler := http.NewServeMux()
+
+	muxHandler.HandleFunc("/", handler)
+
+	server := http.Server{
+		Addr:    ":8085",
+		Handler: muxHandler,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func handler(w http.ResponseWriter, _ *http.Request) {
+
+	fmt.Fprintln(w, "Hello World")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/AhmadKusumahDEV/go-post-micro
+
+go 1.21.5
