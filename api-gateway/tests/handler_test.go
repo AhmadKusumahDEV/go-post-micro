6d7124ec900879d035cbdcd94e39fc848a9b8bd5
MUHAMMAD-FATUR-SOFYAN-KUSUMAH <mfatur.fs@gmail.com>
2025-02-12 22:28:53 +0700

    change interface , reposiotry, services

--- api-gateway/tests/handler_test.go
+++ api-gateway/tests/handler_test.go
@@ -1,12 +1,17 @@
 package tests
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
+	"github.com/AhmadKusumahDEV/go-post-micro/api-gateway/config"
+	"github.com/AhmadKusumahDEV/go-post-micro/api-gateway/internal/domain"
 	"github.com/AhmadKusumahDEV/go-post-micro/api-gateway/internal/product"
 )
 
@@ -22,4 +27,64 @@ func TestHandler(t *testing.T) {
 	body, _ := io.ReadAll(response.Body)
 	fmt.Println(string(body))
 
+}
+
+func TestConvert(t *testing.T) {
+	r := config.InitRedis()
+
+	fmt.Println("test")
+
+	var data []domain.Product
+	resp, err := http.Get("https://mongokopikan.vercel.app/products")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	readAll, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	err = json.Unmarshal(readAll, &data)
+	if err != nil {
+		fmt.Println(err)
+	}
+	r.Set(context.Background(), "babi2", readAll, 5*time.Minute)
+	defer resp.Body.Close()
+	for _, v := range data {
+		fmt.Println("id: ", v.Id)
+		fmt.Println("price: ", v.Price)
+		fmt.Println("desc: ", v.Desc)
+		fmt.Println("image: ", v.Image)
+	}
+
+	dataset, err := r.Get(context.Background(), "babi2").Bytes()
+	if dataset != nil {
+		fmt.Println(dataset)
+	}
+	if err != nil {
+		fmt.Println(err)
+	}
+
+}
+
+func TestGetDataRedis(t *testing.T) {
+	r := config.InitRedis()
+
+	var data []domain.Product
+
+	dataset, err := r.Get(context.Background(), "babi2").Bytes()
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	err = json.Unmarshal(dataset, &data)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	if dataset != nil {
+		fmt.Println(data)
+	}
 }
